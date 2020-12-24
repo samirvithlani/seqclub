b0d7325f633dae85260ee28dd23779e63602b305
=samirvithlani <=samir.vithlani83955@gmail.com>
2020-12-24 13:41:43 +0530

    up

--- controller/EmployeeController.js
+++ controller/EmployeeController.js
@@ -18,3 +18,67 @@ exports.create=(req,res)=>{
     })
 
 }
+
+exports.findAll = (req,res)=>{
+
+    Employee.findAll().then(data=>{
+        res.send(data)
+    }).catch(err=>{
+        res.status(500).send({
+            error:"error while fetching data..."
+        })
+    })
+}
+exports.findOne = (req,res)=>{
+
+    const id = req.params.id
+    Employee.findByPk(id).then(data=>{
+        res.send(data)
+    }).catch(err=>{
+        res.status(500).send({
+            error:"error while fetching data..."
+        })
+    })
+
+}
+exports.update =(req,res)=>{
+
+    const id = req.params.id
+    console.log(id)
+    const employee ={
+
+        name:req.body.name,
+        email:req.body.email,
+        age:req.body.age
+
+    }
+    Employee.update(employee,{where:{id:id}})
+    res.send("ok..")
+}
+exports.delete = (req,res)=>{
+    const id = req.params.id
+    Employee.destroy({
+        where:{id:id}
+    }).then(rs =>{
+        if(rs==1){
+            res.send("deleted")
+        }
+        else{
+            res.send("not deleted..")
+        }
+
+    })
+
+}
+exports.deleteByName = (req,res)=>{
+
+    const name = req.params.name
+    Employee.destroy({where:{name:name}}).then(no=>{
+        if(no>0){
+            res.send("deleted..")
+        }
+        else{
+            res.send("not deleted..")
+        }
+    })
+}
--- routes/employeeRouts.js
+++ routes/employeeRouts.js
@@ -4,5 +4,10 @@ module.exports =app=>{
     var router = require('express').Router();
 
     router.post('/',employee.create)
+    router.get('/',employee.findAll)
+    router.get('/:id',employee.findOne)
+    router.put('/:id',employee.update)
+    router.delete('/:id',employee.delete)
+    router.delete('/byname/:name',employee.deleteByName)
     app.use('/employee',router)
 }
